2d05e76668b97aafa609881333d00d16c3cef269
Niraj Phutane <nirajphutane.np@gmail.com>
2025-02-17 23:14:33 +0530

    Add const

--- lib/StructuralDesignPatterns/DecoratorPattern/Example_2/main.dart
+++ lib/StructuralDesignPatterns/DecoratorPattern/Example_2/main.dart
@@ -3,9 +3,9 @@ import 'WidgetDecorator.dart';
 import 'Widgets.dart';
 
 void main(final List<String> args) {
-  Container(Center(Text('Text-1'))).render();
+  const Container(Center(Text('Text-1'))).render();
 
   print('');
-  final WidgetDecorator decorator = Center(Container(Text('Text-2')));
+  const WidgetDecorator decorator = Center(Container(Text('Text-2')));
   decorator.render();
 }
